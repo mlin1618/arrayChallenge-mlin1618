1d341982ad028e7ef266f172755b840b5fcb644c
mlin1618 <maxlin1234@gmail.com>
2016-10-27 17:52:13 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Nilxam on 10/27/16.
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter how many students: ");
+        int n = scan.nextInt();
+        String[] names = new String[n];
+        int[][] grades = new int[n][1000000];
+        int[] numGrades = new int[n];
+        scan.nextLine();
+        for(int i = 0; i < n; i++){
+            System.out.println("Enter a student's name. ");
+            names[i] = scan.nextLine();
+            System.out.println("Enter a grade. enter -1 to stop");
+            while(true){
+                int temp = scan.nextInt();
+                if(temp==-1) break;
+                grades[i][numGrades[i]] = temp;
+                numGrades[i]++;
+            }
+            scan.nextLine();
+        }
+        for(int i = 0; i < n; i++){
+            double avg = 0;
+            for(int j = 0; j < numGrades[i]; j++){
+                avg += grades[i][j];
+            }
+            System.out.println(names[i] + " " + avg/numGrades[i]);
+        }
+    }
+}
